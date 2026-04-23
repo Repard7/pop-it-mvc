8cf45afe53e3b924cc584385c58ae7b5b3c0d380
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-04-23 22:42:29 +0700

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,25 @@
 
 use Src\Route;
 
-Route::add('GET', '/hello', [Controller\Site::class, 'hello'])
-   ->middleware('auth');
-Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
+Route::add(['GET'], '/', [Controller\Site::class, 'index']);
+
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
-Route::add('GET', '/go', [Controller\Site::class, 'index']);
 
+// Сотрудники
+Route::add('GET', '/employees', [Controller\EmployeeController::class, 'index'])->middleware('auth');
+Route::add(['GET', 'POST'], '/employees/add', [Controller\EmployeeController::class, 'add'])->middleware('auth');
+Route::add(['GET', 'POST'], '/employees/edit', [Controller\EmployeeController::class, 'edit'])->middleware('auth');
+Route::add('GET', '/employees/delete', [Controller\EmployeeController::class, 'delete'])->middleware('auth');
+
+// Кафедры
+Route::add('GET', '/departments', [Controller\DepartmentController::class, 'index'])->middleware('auth');
+Route::add(['GET', 'POST'], '/departments/add', [Controller\DepartmentController::class, 'add'])->middleware('auth');
+Route::add(['GET', 'POST'], '/departments/edit', [Controller\DepartmentController::class, 'edit'])->middleware('auth');
+Route::add('GET', '/departments/delete', [Controller\DepartmentController::class, 'delete'])->middleware('auth');
+
+// Дисциплины
+Route::add('GET', '/disciplines', [Controller\DisciplineController::class, 'index'])->middleware('auth');
+Route::add(['GET', 'POST'], '/disciplines/add', [Controller\DisciplineController::class, 'add'])->middleware('auth');
+Route::add(['GET', 'POST'], '/disciplines/edit', [Controller\DisciplineController::class, 'edit'])->middleware('auth');
+Route::add('GET', '/disciplines/delete', [Controller\DisciplineController::class, 'delete'])->middleware('auth');
